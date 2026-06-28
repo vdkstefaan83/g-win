3533da3c9cae92e1e3f93eda20acba1c9f67fda4
Stefaan <stefaan@vanderkerken.com>
2026-06-28 23:24:30 +0200

    Add gwin.be and www.gwin.be to site domain config

--- app/Config/sites.php
+++ app/Config/sites.php
@@ -5,6 +5,8 @@ return [
     'www.gwin.vanderkerken.com' => ['slug' => 'gwin', 'name' => 'G-Win', 'layout' => 'gwin'],
     'www.g-win.be' => ['slug' => 'gwin', 'name' => 'G-Win', 'layout' => 'gwin'],
     'g-win.be' => ['slug' => 'gwin', 'name' => 'G-Win', 'layout' => 'gwin'],
+    'www.gwin.be' => ['slug' => 'gwin', 'name' => 'G-Win', 'layout' => 'gwin'],
+    'gwin.be' => ['slug' => 'gwin', 'name' => 'G-Win', 'layout' => 'gwin'],
     'localhost' => ['slug' => 'gwin', 'name' => 'G-Win Dev', 'layout' => 'gwin'],
     '127.0.0.1' => ['slug' => 'gwin', 'name' => 'G-Win Dev', 'layout' => 'gwin'],
 ];
